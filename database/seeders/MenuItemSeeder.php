6b638bba40690384dbd9e887164ca8c311143ba0
afifrayhanpranto76-lab <afif.rayhan.pranto76@gmail.com>
2026-05-04 14:21:04 +0600

    fix: add missing DB import in MenuItemSeeder

--- database/seeders/MenuItemSeeder.php
+++ database/seeders/MenuItemSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\MenuItem;
 use App\Models\Role;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MenuItemSeeder extends Seeder
 {
